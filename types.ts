e4ef15775c3a9a9c78b01316e2540a575b85440d
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-12-28 20:44:34 -0300

    Types

---
+++ types.ts
@@ -0,0 +1,53 @@
+// boolean (true/false)
+let isOpen: boolean
+isOpen = true
+
+// string ('foo', "foo", `foo`)
+let message: string
+message = `foo ${isOpen}`
+
+// number (int, float, hex, binary)
+let total: number
+total = 20.3
+total = 0xff0
+
+// array (type[])
+let items: number[]
+items = [1, 2, 3]
+
+let values: Array<number> // generic
+values = [1, 2, 3]
+
+// tuple
+let title: [number, string]
+title = [1, 'foo']
+
+// enum
+enum Colors {
+  white = '#fff',
+  black = '#000'
+}
+
+// any (qualquer coisa)
+let coisa: any
+coisa = [1]
+
+// void (vazio)
+function logger(): void {
+  console.log('foo');
+}
+
+// null | undefined
+type Bla = string | undefined
+
+// never
+function error(): never {
+  throw new Error("error");
+}
+
+// object
+let cart: object
+
+cart = {
+  key: "fi"
+}
